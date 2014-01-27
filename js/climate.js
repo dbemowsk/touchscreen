9e3202266a1de424c828d17ed556800c933274a7
dbemowsk <dbemowsk@phpwebscripting.com>
2014-01-26 23:05:29 -0600

    Moved climate js code to it's own file and got thermostat temp changes and mode/fan changes communicating through websockets

---
+++ js/climate.js
@@ -0,0 +1,247 @@
+// This is an associative array of display names for the available thermostat states
+var states = {'off'        : 'Off',
+              'on'         : 'On',
+              'heat'       : 'Heat',
+              'cool'       : 'Cool',
+              'auto'       : 'Auto',
+              'emerg_heat' : 'Emergency Heat',
+              'invalid'    : 'Invalid',
+              'fan_on'     : 'On',
+              'fan_auto'   : 'Auto',
+              'hold'       : 'Hold',
+              'run'        : 'Run',
+              'vacation'   : 'Vacation',
+              'no_vacation': 'Vacation Off'};
+
+// This is a list of the switchable modes used for mode selection
+var modes = ['off', 'heat', 'cool', 'auto'];
+
+// This is a list of the switchable fan modes used for fan mode selection
+var fanModes = ['fan_auto', 'fan_on'];
+
+$(function() {
+  //Add an onClick handler for selecting modes
+  $('#mode_select').click(function() { 
+    var val = GetKeyFromHash(states, $('#mode_select_val').text());
+    var idx = modes.indexOf(val);
+    idx++;
+    idx = (idx > modes.length - 1) ? 0 : idx;
+    $('#mode_select_val').text(states[modes[idx]]);
+
+    //Here we need to send the selected mode value to MH
+    var msg = {
+      type: "click",
+      text: "therm_mode",
+      value: modes[idx]
+    };
+    //Send the command
+    $.ts.webSocket.sendToMH(msg);
+  });
+
+  //Add an onClick handler for selecting fan modes
+  $('#fan_select').click(function() {
+    var fansel = GetKeyFromHash(states, $('#fan_select_val').text());
+    var idx = fanModes.indexOf(fansel);
+    idx++;
+    idx = (idx > fanModes.length - 1) ? 0 : idx;
+    $('#fan_select_val').text(states[fanModes[idx]]);
+
+    //Here we need to send the selected fan mode value to MH
+    var msg = {
+      type: "click",
+      text: "therm_fan",
+      value: fanModes[idx]
+    };
+    //Send the command
+    $.ts.webSocket.sendToMH(msg);
+  });
+
+  updateThermInfo();
+});
+
+//******************************************************************************
+// This function is used to get a key name from a value in a hash
+//
+// @arg hash - The hash containing the key value pairs
+// @arg val - The value to search for in the hash
+//
+// return the key name for the value in the hash or unknown if not found
+//******************************************************************************
+function GetKeyFromHash(hash, val) {
+  var rtn = "unknown";
+  for (var name in hash) {
+    if (hash[name] === val) rtn = name;
+  }
+  return rtn;
+}
+
+//******************************************************************************
+// This function takes a temperature value and returns a hex rgb color value
+// that relates to that temperature.  Colder values return cooler colors in the  
+// blue spectrum, and hotter values return warmer colors in the red spectrum. 
+//
+// @arg temp - The temperature value to convert
+// @arg fc - farenheit or celsius
+//******************************************************************************
+function Temp2RGB(temp, fc) {
+  //Convert to farenheit if necessary
+  temp = (fc == "C" || fc == "c") ? ((temp * 9 / 5) + 32) : temp;
+
+  var RGB = '#000034';
+  if (temp > 99) { RGB = '#670001'; } else
+  if (temp > 91.5) { RGB = '#800000'; } else
+  if (temp > 83.1) { RGB = '#990002'; } else
+  if (temp > 74.6) { RGB = '#c22009'; } else
+  if (temp > 66.1) { RGB = '#ee540e'; } else
+  if (temp > 57.7) { RGB = '#fdca00'; } else
+  if (temp > 49.2) { RGB = '#fcff68'; } else
+  if (temp > 40.8) { RGB = '#fefefe'; } else
+  if (temp > 32.3) { RGB = '#99cdff'; } else
+  if (temp > 23.8) { RGB = '#3c9dd2'; } else
+  if (temp > 15.4) { RGB = '#0066cb'; } else
+  if (temp > 6.9) { RGB = '#003466'; } else
+  if (temp > -1.5) { RGB = '#003466'; } 
+    
+  return RGB;
+}
+
+//******************************************************************************
+// This function gets the current thermostat information and updates the page
+//
+//******************************************************************************
+function updateThermInfo() {
+  var url;
+
+  //Get updated thermostat information every 15 seconds
+  //window.setTimeout(updateThermInfo, 15000);
+
+  url = './sub?json(subs=therm_get_mode)';
+
+  //Get the current thermostat mode
+  $.getJSON(url, function (data) {
+    mode = data.subs.therm_get_mode;
+  $('#mode_select_val').html(states[mode]);
+  });
+
+  url = './sub?json(subs=therm_get_fan_mode)';
+
+  //Get the current fan mode
+  $.getJSON(url, function (data) {
+    mode = data.subs.therm_get_fan_mode;  
+  
+    $('#fan_select_val').html(states[mode]);
+  });
+
+  url = './sub?json(subs=therm_get_heat_setpoint)';
+
+  //Get the current heating setpoint
+  $.getJSON(url, function (data) {
+    hsp = data.subs.therm_get_heat_setpoint;
+  
+    //Update the spinner's valuenow and set the displayed heating setpoint  
+    $('#sb1').attr('aria-valuenow', hsp);
+    $('#sb1').html(hsp);
+    //Once we update the values we need to start the spinbutton controls
+    if (typeof spin1 != 'object') {
+      var spin1 = new spinbutton('sb1', 'sb1_up', 'sb1_down', 1, function() {
+        //Here we need to send the heating setpoint value to MH
+        var msg = {
+          type: "click",
+          text: "hsp",
+          value: this.valNow
+        };
+        //Send the command
+        $.ts.webSocket.sendToMH(msg);
+      });
+    }
+  });
+
+  url = './sub?json(subs=therm_get_cool_setpoint)';
+
+  //Get the current cooling setpoint
+  $.getJSON(url, function (data) {
+    csp = data.subs.therm_get_cool_setpoint;
+
+    //Update the spinner's valuenow and set the displayed heating setpoint
+    $('#sb2').attr('aria-valuenow', csp);  
+    $('#sb2').html(csp);
+    //Once we update the values we need to start the spinbutton controls
+    if (typeof spin2 != 'object') {
+      var spin2 = new spinbutton('sb2', 'sb2_up', 'sb2_down', 1, function() {
+        //Here we need to send the cooling setpoint value to MH
+        var msg = {
+          type: "click",
+          text: "csp",
+          value: this.valNow
+        };
+        //Send the command
+        $.ts.webSocket.sendToMH(msg);
+      });
+    }
+  });
+}
+
+//******************************************************************************
+// This function gathers the weather data and displays it on the right half of 
+// the climate page.
+//
+//******************************************************************************
+function updateWeather() {
+  //Get updated weather information every 30 seconds
+  window.setTimeout(updateWeather, 30000);
+
+  var url = './sub?json(weather)';
+  var out = '';
+  var cloudImg = "";
+
+  //get the weather data
+  $.getJSON(url, function (data) {
+    //we will get the US (F) or metric (C) value from the Summary_Short 
+    var fc = data.Weather.Summary_Short.match(/^-?\d*\.?\d*?deg\;(F|C)/)[1];
+
+    //We should not use the "Clouds" value if we have a "Condition" value set
+    var conditionIsSet = false;
+
+    //Now loop through the weather data
+    $.each(data.Weather, function(key, value) {
+      //check if we have a page element to update
+      var el = $('#' + key);
+      out += "["+key+"] = "+value+"\n";
+      if (el.length > 0) {
+        value = (value == "null") ? "?" : value;
+        if (key.match(/^Temp/) || key.match(/^WindChill$/)) {
+          el.html(value + " &deg;" + fc);
+          el.css('color', Temp2RGB(value, fc));
+        } else if (key.match(/^Conditions$/)) {
+          //if the returned condition text contains spaces, replace them with underscores
+          value = value.replace(/ /, "_");
+          SetWeatherImage(key,value);
+          conditionIsSet = true;
+        } else if (key.match(/^HumidOutdoor$/)) {
+          el.html(value + " %");
+        } else if (key.match(/^BaromSea$/)) {
+          el.html(value + " inches and " + data.Weather.BaromDelta);
+        } else {
+          el.html(value);
+        }
+      }
+      if (key.match(/^Clouds/)) {
+          cloudImg = value.replace(/ /, "_");
+          cloudEle = key;
+        }
+    });
+
+    //If all is done and there is not a condition image being displayed, display a clouds image if one exists
+    if (!conditionIsSet && cloudImg != "") {
+     SetWeatherImage(cloudEle,cloudImg);
+    } 
+  });
+}
+
+function SetWeatherImage(el,value) {
+  el = $('#Conditions');
+  el.css('background-image', 'url(./images/weather/' + value + '.png)');
+  el.css('background-size', '256px 256px');
+  el.css('background-repeat', 'no-repeat');
+  el.css('background-position', 'center 20px');
+}
--- js/main.js
+++ js/main.js
@@ -1,7 +1,5 @@
+//This holds the current page that we are on
 var curr_page = (typeof curr_page == 'undefined') ? "main_menu" : curr_page;
-
-//Update the MisterHouse mode icon in the upper left corner every minute
-window.setInterval(function() { GetMhMode('header'); }, 60000);
 
 //Send the device back to the main menu page after 10 minutes of inactivity
 var IDLE_TIMEOUT = 300; //seconds
@@ -11,14 +9,20 @@ var _idleSeconds = 0;
 document.onclick = function() {
     _idleSeconds = 0;
 };
+
 document.onmousemove = function() {
     _idleSeconds = 0;
 };
+
 document.onkeypress = function() {
     _idleSeconds = 0;
 };
+
 //Check the idle time and update the time in the upper right corner every second
 window.setInterval(CheckIdleTime, 1000);
+
+//Update the MisterHouse mode icon in the upper left corner every minute
+window.setInterval(function() { GetMhMode('header'); }, 60000);
 
 //******************************************************************************
 // This function is the handler for the idle time counter. This gets run once 
@@ -44,10 +48,15 @@ function CheckIdleTime() {
 }
 
 //******************************************************************************
-// This function is the jquery .ready function. Here we load the initial page 
-// and get the current system mode icon and display it in the header
+// This function is the jquery .ready function. Here we initialize the webSocket
+// load the initial page and get the current system mode icon and display it in 
+// the header
 //******************************************************************************
 $(function() {
+  //App globals and settings
+  $.ts = { };
+  //Initialize webSocket object
+  initWebSocket();
   //Load the initial page
   LoadPage(curr_page);
   //Get the current mh mode and display it
@@ -78,101 +87,6 @@ function ShowTime() {
 function GoPage(page_name) {
   curr_page = page_name;
   LoadPage(page_name);
-}
-
-//******************************************************************************
-// This function takes a temperature value and returns a hex rgb color value
-// that relates to that temperature.  Colder values return cooler colors in the  
-// blue spectrum, and hotter values return warmer colors in the red spectrum. 
-//
-// @arg temp - The temperature value to convert
-// @arg fc - farenheit or celsius
-//******************************************************************************
-function Temp2RGB(temp, fc) {
-  //Convert to farenheit if necessary
-  temp = (fc == "C" || fc == "c") ? ((temp * 9 / 5) + 32) : temp;
-  
-  var RGB = '#000034';
-  if (temp > 99) { RGB = '#670001'; } else
-  if (temp > 91.5) { RGB = '#800000'; } else
-  if (temp > 83.1) { RGB = '#990002'; } else
-  if (temp > 74.6) { RGB = '#c22009'; } else
-  if (temp > 66.1) { RGB = '#ee540e'; } else
-  if (temp > 57.7) { RGB = '#fdca00'; } else
-  if (temp > 49.2) { RGB = '#fcff68'; } else
-  if (temp > 40.8) { RGB = '#fefefe'; } else
-  if (temp > 32.3) { RGB = '#99cdff'; } else
-  if (temp > 23.8) { RGB = '#3c9dd2'; } else
-  if (temp > 15.4) { RGB = '#0066cb'; } else
-  if (temp > 6.9) { RGB = '#003466'; } else
-  if (temp > -1.5) { RGB = '#003466'; } 
-  
-  return RGB;
-}
-
-//******************************************************************************
-// This function gathers the weather data and displays it on the right half of 
-// the climate page.
-//
-//******************************************************************************
-function updateWeather() {
-  //Get updated weather information every 30 seconds
-  window.setTimeout(updateWeather, 30000);
-
-  var url = './sub?json(weather)';
-  var out = '';
-  var cloudImg = "";
-
-  //get the weather data
-  $.getJSON(url, function (data) {
-    //we will get the US (F) or metric (C) value from the Summary_Short 
-    var fc = data.Weather.Summary_Short.match(/^-?\d*\.?\d*?deg\;(F|C)/)[1];
-
-    //We should not use the "Clouds" value if we have a "Condition" value set
-    var conditionIsSet = false;
-
-    //Now loop through the weather data
-    $.each(data.Weather, function(key, value) {
-      //check if we have a page element to update
-      var el = $('#' + key);
-      out += "["+key+"] = "+value+"\n";
-      if (el.length > 0) {
-        value = (value == "null") ? "?" : value;
-        if (key.match(/^Temp/) || key.match(/^WindChill$/)) {
-          el.html(value + "&deg; " + fc);
-          el.css('color', Temp2RGB(value, fc));
-        } else if (key.match(/^Conditions$/)) {
-          //if the returned condition text contains spaces, replace them with underscores
-          value = value.replace(/ /, "_");
-          SetWeatherImage(key,value);
-          conditionIsSet = true;
-        } else if (key.match(/^HumidOutdoor$/)) {
-          el.html(value + " %");
-        } else if (key.match(/^BaromSea$/)) {
-          el.html(value + " inches and " + data.Weather.BaromDelta);
-        } else {
-          el.html(value);
-        }
-      }
-      if (key.match(/^Clouds/)) {
-          cloudImg = value.replace(/ /, "_");
-          cloudEle = key;
-        }
-    });
-
-    //If all is done and there is not a condition image being displayed, display a clouds image if one exists
-    if (!conditionIsSet && cloudImg != "") {
-       SetWeatherImage(cloudEle,cloudImg);
-    } 
-  });
-}
-
-function SetWeatherImage(el,value) {
-  el = $('#Conditions');
-  el.css('background-image', 'url(./images/weather/' + value + '.png)');
-  el.css('background-size', '256px 256px');
-  el.css('background-repeat', 'no-repeat');
-  el.css('background-position', 'center 20px');
 }
 
 //******************************************************************************
@@ -255,4 +169,101 @@ function formatAMPM() {
   days = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
   
   return days[d.getDay()]+' '+months[d.getMonth()]+' '+d.getDate()+' '+d.getFullYear()+' '+hours+':'+minutes+ampm;
+}
+
+
+//******************************************************************************
+// This is to hijack the javascript alert function to place alerts on the bottom 
+// status bar.  It also sets a timeout function that will clear the status bar 
+// after a defined timeout period
+//
+// @arg msg - The message to be displayed.
+// @arg timeout - An optional timeout period can be passed to tell how long the 
+//                message should stay in the status bar before being cleared. if
+//                no timeout is specified, the default is 20 seconds.
+//******************************************************************************
+function alert(msg, timeout) {
+  // Set the timeout value
+  timeout = timeout || 20000
+  // Display the message
+  $('#AlertMsg').html(msg);
+  setTimeout(function() { $('#AlertMsg').html('&nbsp;'); }, timeout);
+}
+
+//******************************************************************************
+// webSocket object
+//
+// define some constants for later use
+// initialize webSocket object and methods
+// create a websocket connection and define its event functions
+//
+// Sean Mathews
+// coder at f34r dot com
+//
+//******************************************************************************
+function initWebSocket()
+{
+  // create our connection url and add to our touchscreen(ts) object
+  $.ts.wsUri = "ws://" + window.location.hostname + ":3000/";
+
+  // start the connection. async so it wont happen just yet
+  $.ts.webSocket = new WebSocket($.ts.wsUri);
+
+  // connect establish event
+  $.ts.webSocket.onopen = function(evt) {
+    console.log("WebSocket connected");
+  };
+
+  // connection close event
+  $.ts.webSocket.onclose = function(evt) {
+    console.log("WebSocket closed");
+  };
+
+  // connection message event
+  $.ts.webSocket.onmessage = function(evt) {
+    console.log("WebSocket message: " + evt.data);
+    alert("WebSocket message: " + evt.data);
+    // check for a restart event
+    if(evt.data === "RESTART") {
+       alert("MH just told me it restarted. Is all ok?");
+    }
+
+    //check for a thermostat mode event
+    if(therm_mode = /^THERM_MODE:(.*)/.exec(evt.data)) {
+      if (val = $('#mode_select_val')) {
+        if (val.text() != states[therm_mode[1]]) {
+          val.text(states[therm_mode[1]]);
+          alert("Mode changed to " + states[therm_mode[1]]);
+        }
+      }
+    }
+
+    //check for a thermostat fan event
+    if(therm_fan = /^THERM_FAN:(.*)/.exec(evt.data)) {
+      if (val = $('#fan_select_val')) {
+        if (val.text() != states[therm_fan[1]]) {
+          val.text(states[therm_fan[1]]);
+          alert("Fan mode changed to " + states[therm_fan[1]]);
+        }
+      }
+    }
+  };
+
+  // connection error event
+  $.ts.webSocket.onerror = function(evt) {
+    console.log("WebSocket error");
+  };
+
+  // sendToMH send a json message to MH
+  //
+  // var msg = {
+  // event: "click",
+  // data: "power"
+  // };
+  // $.ts.webSocket.sendToMH(msg);
+  //
+  $.ts.webSocket.sendToMH = function (msg) {
+    console.log("WebSocket sendToMH");
+    $.ts.webSocket.send(JSON.stringify(msg));
+  };
 }
